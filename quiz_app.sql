9a3572577177d6cb390760696978974af2dcc38c
Marián Zelinka <maikelekmain@gmail.com>
2023-11-16 23:29:28 +0100

    ✨Added database

---
+++ quiz_app.sql
@@ -0,0 +1,70 @@
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+CREATE TABLE `options` (
+  `option_id` int(11) NOT NULL,
+  `question_id` int(11) NOT NULL,
+  `option_a` varchar(255) NOT NULL,
+  `option_b` varchar(255) NOT NULL,
+  `option_c` varchar(255) NOT NULL,
+  `option_correct` varchar(1) NOT NULL DEFAULT 'A'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+CREATE TABLE `questions` (
+  `question_id` int(11) NOT NULL,
+  `quiz_id` int(11) NOT NULL,
+  `question_text` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+CREATE TABLE `quizes` (
+  `quiz_id` int(11) NOT NULL,
+  `quiz_title` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+CREATE TABLE `stats` (
+  `stat_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `quiz_id` int(11) NOT NULL,
+  `stat_correct_answers` int(11) NOT NULL,
+  `stat_time` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+CREATE TABLE `users` (
+  `user_id` int(11) NOT NULL,
+  `user_name` varchar(30) NOT NULL,
+  `user_email` int(60) NOT NULL,
+  `user_password` varchar(255) NOT NULL,
+  `user_is_admin` int(1) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+ALTER TABLE `options`
+  ADD PRIMARY KEY (`option_id`);
+
+ALTER TABLE `questions`
+  ADD PRIMARY KEY (`question_id`);
+
+ALTER TABLE `quizes`
+  ADD PRIMARY KEY (`quiz_id`);
+
+ALTER TABLE `stats`
+  ADD PRIMARY KEY (`stat_id`);
+
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`user_id`);
+
+ALTER TABLE `options`
+  MODIFY `option_id` int(11) NOT NULL AUTO_INCREMENT;
+
+ALTER TABLE `questions`
+  MODIFY `question_id` int(11) NOT NULL AUTO_INCREMENT;
+
+ALTER TABLE `quizes`
+  MODIFY `quiz_id` int(11) NOT NULL AUTO_INCREMENT;
+
+ALTER TABLE `stats`
+  MODIFY `stat_id` int(11) NOT NULL AUTO_INCREMENT;
+
+ALTER TABLE `users`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
